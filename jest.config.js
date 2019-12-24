5e0775b9135cfb040f2d9dff88428e05c2dbf7a6
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2019-12-24 18:41:25 +0100

    Now i have jest and testing library support HO HO HO

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleDirectories: [
+    "app/javascript","node_modules"
+  ], 
+  roots: ["test/javascript"]
+};
---
+++ test/javascript/things.test.tsx
@@ -0,0 +1,5 @@
+describe("True things", () => {
+  it("be true", () => {
+    expect(true).toBe(true);
+  });
+});
