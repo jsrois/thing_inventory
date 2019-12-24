6dfbc693b823dbe4d94a2219ea75d407ab521560
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2019-12-25 00:05:26 +0100

    Added database of things

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,10 +1,5 @@
 class ThingsController < ApplicationController
   def index
-    render json: {
-      things: [
-        { name: 'Camiseta', price: '12.30' },
-        { name: 'Octaveta', price: '0' }
-      ]
-    }
+    render json: { things: Thing.all }
   end
 end
