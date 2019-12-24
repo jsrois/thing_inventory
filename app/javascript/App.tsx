922d5078ebfaf07c7b631d857f5f865a8eb3c076
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2019-12-24 18:25:58 +0100

    Rails 6 app with React and Typescript (no react-rails plugins for now)

---
+++ app/javascript/App.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+const App = (): JSX.Element => <div>Welcome to our React single page application</div>;
+export default App;
---
+++ app/javascript/packs/react_spa.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "../App"
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <App />,
+      document.body.appendChild(document.createElement('div')),
+    )
+  })
